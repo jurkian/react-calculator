14be8a4d8d177ead3fbe088e29be4a829a9580be
Jakub Jurkian <jakub@jurkian.com>
2017-06-04 23:21:56 +0200

    Add Normalize.css and set border-box

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import 'normalize.css';
+import './index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/index';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
