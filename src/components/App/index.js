ed61e2163130872e952a5edbdb9231586b520262
Jakub Jurkian <jakub@jurkian.com>
2017-06-04 21:53:36 +0200

    Optimize structure, build Sass styles

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from '../../images/logo.svg';
-import '../../styles/app.css';
+import logo from './images/logo.svg';
+import './app.css';
 
 class App extends Component {
   render() {
