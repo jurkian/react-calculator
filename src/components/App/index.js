e9c5699d74130c054861b489c5272570cd17d773
Jakub Jurkian <jakub@jurkian.com>
2017-06-04 22:58:47 +0200

    Add primary styles

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,13 +6,39 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <header>
+          <ul className="App-bar-icons">
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+
+          <span className="App-history">33x2</span>
+          <span className="App-result">66</span>
+        </header>
+
+        <section className="App-panel">
+          <ul>
+            <li className="highlight">C</li>
+            <li>%</li>
+            <li>/</li>
+            <li>X</li>
+            <li>7</li>
+            <li>8</li>
+            <li>9</li>
+            <li>-</li>
+            <li>4</li>
+            <li>5</li>
+            <li>6</li>
+            <li>+</li>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>0</li>
+            <li>.</li>
+            <li className="highlight">=</li>
+          </ul>
+        </section>
       </div>
     );
   }
